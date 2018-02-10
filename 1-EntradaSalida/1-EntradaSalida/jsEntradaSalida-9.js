43b60e067b849275064909bf034e17acb2779a22
Urrels <36246890+Urrels@users.noreply.github.com>
2018-02-10 12:29:15 -0300

    Add files via upload

--- 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,12 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+var sueldo;
+var nuevosueldo; 
 
-	
+sueldo = parseInt(document.getElementById("sueldo").value);
+
+nuevosueldo = sueldo * 1.1;
+
+document.getElementById("resultado").value = nuevosueldo.toFixed(2);
 }
