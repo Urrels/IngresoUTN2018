d68e9a1ab446ba810ed19e82d0fd0282463b4b4f
Urrels <36246890+Urrels@users.noreply.github.com>
2018-02-10 23:23:48 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
-
-
+var edad;
+edad =document.getElementById("edad").value;
+if (edad<12 || edad>17)
+{
+    if(edad<12)
+    {
+        alert ("¡Usté es un niño!");
+    }
+    if (edad>17)
+    {
+        alert ("¡Usté es mayor de edad!");
+    }
+}
+else
+{
+    alert ("¡Usté es adolescente!");
+}
 }//FIN DE LA FUNCIÓN
