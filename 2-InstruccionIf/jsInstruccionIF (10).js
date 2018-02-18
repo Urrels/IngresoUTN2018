a288c9fd9c7215cb31bef0400a755a1b44e559ec
Urrels <36246890+Urrels@users.noreply.github.com>
2018-02-18 12:29:27 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroaleatorio;
+	numeroaleatorio = Math.floor(Math.random()*10)+1;
+
+	if (numeroaleatorio==9 || numeroaleatorio==10)
+	{
+		alert ("EXCELENTE " + numeroaleatorio);
+	}
+	else if (numeroaleatorio>3)
+	{
+		alert("APROBÓ " + numeroaleatorio);
+
+	}
+
+	else
+	{
+		alert("Vamos, la próxima se puede " + numeroaleatorio);
+	}
 
 }//FIN DE LA FUNCIÓN
+
+
+
+
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,15 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=parseInt(edad =document.getElementById("edad").value);
+estadocivil=document.getElementById("estadoCivil").value;
 
-	
+    if (edad<18 && estadocivil != document.getElementById("Soltero"))
+    {
+        alert ("Es muy pequeño para NO ser soltero");
+    }
+        
 
 
 }//FIN DE LA FUNCIÓN
