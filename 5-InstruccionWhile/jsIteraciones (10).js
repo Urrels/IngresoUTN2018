9e046f60fcb3e2170db5a5cf01018996affa218f
Urrels <36246890+Urrels@users.noreply.github.com>
2018-03-02 00:04:22 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,62 @@
 function Mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	var negativos;  
+	var positivos;
+	var promedionegativos;
+	var promediopositivos;
+	var contadorceros;
+	var contadornegarivos=0;
+	var contadorpositivos;
+	var contadornumerospar;
+	var numero=0;
+	var respuesta;
+	var diferencia;
+	var acumuladorpos;
+	var acumuladorneg;
+do
 	{
-		
-	
+	numero= parseInt(prompt("Ingrese un número"));
+	if (numero>0)
+	{
+		contador= contador + 1;
+		acumuladorpos= acumuladorpos + 1;
 	}
+		else if (numero<0)
+		{
+			
+		}
 
+		else
+		{
+			contadorceros= contadorceros + 1;
+		}
 
+		if (numero/2==0)
+		{
+			contadornumerospar = contadornumerospar + 1;
+		}
 
+	respuesta=prompt("¿Desea continuar?");
+	
+	} while (respuesta == 's');
+
+promediopositivos=acumuladorpos/promediopositivos;
+promedionegativos=acumuladorneg/promedionegativos;
+diferencia=contadorpositivos/contadornegativos;
+
+document.write("1-suma de los negativos:_" + acumuladorneg + "<br/>");
+document.write("2-Suma de los positivos:_" + acumuladorpos + "<br/>"); 
+
+if (contadorpositivos != 0)
+{
+	promediopositivos=acumuladorpos/promediopositivos;
+
+}
+ 
+if (contadornegativos!=0)
+{
+	promedionegativos= acumuladorneg/promedionegativos
+}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,29 @@
 function Mostrar()
 {
-
-	var contador=0;
-	// declarar variables
+	var maximo=0;
+	var minimo=0;
+	var respuesta;
 	
-	var respuesta='si';
-
-	while(respuesta!='no')
+	do
 	{
+		numero = parseInt(prompt("Ingrese un número"));
 		
+	if (maximo < numero)
+	{
 	
+		maximo=numero;
 	}
+		else 
+		{
+			minimo=numero;
 
+		} 
+	
+		respuesta = prompt("¿Desea continuar?");
 
+	} while (respuesta == 's');
 
+document.getElementById("maximo").value = maximo;
+document.getElementById("minimo").value = minimo;
 
 }//FIN DE LA FUNCIÓN
